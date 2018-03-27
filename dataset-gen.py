9e9c6758c42912e441a93846e8a3cda3191324e8
Madhurjya Roy <mroy@localhost.localdomain>
2018-03-27 06:54:52 +0530

    update

--- dataset-gen.py
+++ dataset-gen.py
@@ -3,4 +3,40 @@ import time
 from collections import namedtuple
 import pandas as pd
 import requests
-import matplotlib.pyplot as plt
+
+API_KEY = "" # enter API key
+BASE_URL = "http://api.wunderground.com/api/{}/history_{}/q/India/Bhubaneswar.json"
+
+target_date = datetime(2016, 6, 30)
+features = ["date", "meantempm", "meandewptm", "meanpressurem", "maxhumidity", "minhumidity", "maxtempm",
+            "mintempm", "maxdewptm", "mindewptm", "maxpressurem", "minpressurem", "precipm"]
+DailySummary = namedtuple("DailySummary", features)
+
+def extract_weather_data(url, api_key, target_date, days):  
+    records = []
+    for _ in range(days):
+        request = BASE_URL.format(API_KEY, target_date.strftime('%Y%m%d'))
+        response = requests.get(request)
+        if response.status_code == 200:
+            data = response.json()['history']['dailysummary'][0]
+            records.append(DailySummary(
+                date=target_date,
+                meantempm=data['meantempm'],
+                meandewptm=data['meandewptm'],
+                meanpressurem=data['meanpressurem'],
+                maxhumidity=data['maxhumidity'],
+                minhumidity=data['minhumidity'],
+                maxtempm=data['maxtempm'],
+                mintempm=data['mintempm'],
+                maxdewptm=data['maxdewptm'],
+                mindewptm=data['mindewptm'],
+                maxpressurem=data['maxpressurem'],
+                minpressurem=data['minpressurem'],
+                precipm=data['precipm']))
+        time.sleep(6)
+        target_date += timedelta(days=1)
+    return records
+
+records = extract_weather_data(BASE_URL, API_KEY, target_date, 1000)
+df = pd.DataFrame(records, columns=features).set_index('date')  
+df.to_csv("dataset.csv")
