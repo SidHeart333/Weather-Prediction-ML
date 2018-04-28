d4564e4fb8a89b27d55b71f837ddffa2b44f02f6
Vishwanath Sah <vishu.sah404@gmail.com>
2018-04-29 05:07:29 +0530

    linear reg

---
+++ applying-regression.py
@@ -0,0 +1,86 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+dataframe = pd.read_csv('dataset-cleaned.csv').set_index('date')
+
+dataframe.corr()[['meantempm']].sort_values('meantempm')
+
+
+predictors = ['meantempm_1',  'meantempm_2',  'meantempm_3', 
+              'mintempm_1',   'mintempm_2',   'mintempm_3',
+              'meandewptm_1', 'meandewptm_2', 'meandewptm_3',
+              'maxdewptm_1',  'maxdewptm_2',  'maxdewptm_3',
+              'mindewptm_1',  'mindewptm_2',  'mindewptm_3',
+              'maxtempm_1',   'maxtempm_2',   'maxtempm_3']
+
+dataframe2 = dataframe[['meantempm'] + predictors]
+
+# manually set the parameters of the figure to and appropriate size
+plt.rcParams['figure.figsize'] = [16, 22]
+
+# call subplots specifying the grid structure we desire and that 
+# the y axes should be shared
+fig, axes = plt.subplots(nrows=6, ncols=3, sharey=True)
+
+# Since it would be nice to loop through the features in to build this plot
+# let us rearrange our data into a 2D array of 6 rows and 3 columns
+arr = np.array(predictors).reshape(6, 3)
+
+# use enumerate to loop over the arr 2D array of rows and columns
+# and create scatter plots of each meantempm vs each feature
+for row, col_arr in enumerate(arr):
+    for col, feature in enumerate(col_arr):
+        axes[row, col].scatter(dataframe2[feature], dataframe2['meantempm'])
+        if col == 0:
+            axes[row, col].set(xlabel=feature, ylabel='meantempm')
+        else:
+            axes[row, col].set(xlabel=feature)
+plt.show()
+
+import statsmodels.api as sm
+X = dataframe2[predictors]
+y = dataframe2['meantempm']
+
+X = sm.add_constant(X)
+X.iloc[:5, :5]
+
+def backwardElimination(x, sl):
+    numVars = x.shape[1]
+    for i in range(0, numVars):
+        regressor_OLS = sm.OLS(y, x).fit()
+        maxVar = max(regressor_OLS.pvalues).astype(float)
+        if maxVar > sl:
+            for j in range(0, numVars - i):
+                if (regressor_OLS.pvalues[j].astype(float) == maxVar):
+                    x = x.drop(x.columns[j],axis=1)
+                    print(x.columns[0])
+    regressor_OLS.summary()
+    return x
+
+X2 = backwardElimination(X, 0.05)
+X = X2
+
+
+model = sm.OLS(y, X2).fit()
+model.summary()
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=12)
+
+from sklearn.linear_model import LinearRegression
+
+# instantiate the regressor class
+regressor = LinearRegression()
+
+# fit the build the model by fitting the regressor to the training data
+regressor.fit(X_train, y_train)
+
+# make a prediction set using the test set
+prediction = regressor.predict(X_test)
+
+# Evaluate the prediction accuracy of the model
+from sklearn.metrics import mean_absolute_error, median_absolute_error
+print("The Explained Variance: %.2f" % regressor.score(X_test, y_test))
+print("The Mean Absolute Error: %.2f degress celcius" % mean_absolute_error(y_test, prediction))
+print("The Median Absolute Error: %.2f degrees celcius" % median_absolute_error(y_test, prediction))
